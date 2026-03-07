563adce366f862dab83c7ff94d82c606df112f15
MUNNA <munna7777777.1.0@gmail.com>
2026-03-07 19:40:52 +0530

    add kafka

---
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,35 @@
+package grpc;
+
+
+import billing.BillingRequest;
+import billing.BillingResponse;
+import billing.BillingServiceGrpc.BillingServiceImplBase;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.LoggerFactory;
+
+import org.slf4j.Logger;
+
+@GrpcService
+public class BillingGrpcService extends BillingServiceImplBase {
+
+    private static final Logger log = LoggerFactory.getLogger(
+            BillingGrpcService.class
+    );
+
+    @Override
+    public void createBillingAccount(BillingRequest billingRequest, StreamObserver<BillingResponse> responseObserver) {
+
+        log.info("createBillingAccount request received for patientId: {}", billingRequest.toString());
+
+        BillingResponse response = BillingResponse.newBuilder()
+                .setAccountId("123456")
+                .setStatus("ACTIVE")
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+
+
+    }
+}
